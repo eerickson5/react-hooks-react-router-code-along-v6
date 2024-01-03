324e6b8340b007e8fc88ea2df0b5470dd99ceefc
Elise Erickson <elisece717@gmail.com>
2024-01-03 09:52:16 -0500

    makes user profile page dynamic

--- src/pages/UserProfile.js
+++ src/pages/UserProfile.js
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom"
 import NavBar from "../components/NavBar";
 
 export default function UserProfile(){
+  const [user, setUser] = useState({})
+  const userId = useParams().id;
+
+  useEffect(() =>{
+    fetch(`http://localhost:4000/users/${userId}`)
+    .then(r => r.json())
+    .then(data => setUser(data))
+    .catch(error => console.error(error))
+  }, [userId]);
+
+  if(!user.name){
+    return <h1>Loading...</h1>
+  }
     return(
         <>
           <header>
             <NavBar />
           </header>
           <main>
-            <h1>User Profile</h1>
+            <h1>{user.name}</h1>
           </main>
         </>
       );
