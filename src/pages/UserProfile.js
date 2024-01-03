830576e70fe3556aab64b281bb9377bb0214761e
Elise Erickson <elisece717@gmail.com>
2024-01-03 09:16:55 -0500

    adds static user card routing

---
+++ src/pages/UserProfile.js
@@ -0,0 +1,15 @@
+import React from "react";
+import NavBar from "../components/NavBar";
+
+export default function UserProfile(){
+    return(
+        <>
+          <header>
+            <NavBar />
+          </header>
+          <main>
+            <h1>User Profile</h1>
+          </main>
+        </>
+      );
+}
